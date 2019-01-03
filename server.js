16b3f60d51d4d34a4c15f6a92d5cecc9bb83b0ec
Aaron Deane <aaronmdeane@gmail.com>
2019-01-02 17:20:54 -0800

    Proxy server successfully serving up Photos component

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/watches/:wid', express.static(path.join(__dirname, 'public')));
+app.use('/api/watches/:wid/photos', proxy({ target: 'http://127.0.0.1:3001', changeOrigin: true }));
 
 app.listen(PORT, () => console.log(`Express Proxy listening on port ${PORT}`));
