d22cc0b7517e2ba2dd70e82a623328da9f2759ac
Aaron Deane <aaronmdeane@gmail.com>
2019-01-02 16:55:40 -0800

    Initial proxy server setup and serving static message

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log(`Express Proxy listening on port ${PORT}`));
