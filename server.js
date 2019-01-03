0c32d515254964664d7ec7d4fc4f39a6bd389ff3
Aaron Deane <aaronmdeane@gmail.com>
2019-01-02 19:11:55 -0800

    Added Summary component to proxy

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const PORT = 3000;
 
 app.use(morgan('dev'));
 app.use('/watches/:wid', express.static(path.join(__dirname, 'public')));
+
 app.use('/api/watches/:wid/photos', proxy({ target: 'http://127.0.0.1:3001', changeOrigin: true }));
+app.use('/api/watches/:wid/summary', proxy({ target: 'http://127.0.0.1:3002', changeOrigin: true }));
 
 app.listen(PORT, () => console.log(`Express Proxy listening on port ${PORT}`));
