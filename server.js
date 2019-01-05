8e3b2e6126bc0329785814188034fbdd2fbba382
Aaron Deane <aaronmdeane@gmail.com>
2019-01-04 20:38:01 -0800

    Setup Docker for proxy deployment

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const PORT = 3000;
 app.use(morgan('dev'));
 app.use('/watches/:wid', express.static(path.join(__dirname, 'public')));
 
-app.use('/api/watches/:wid/photos', proxy({ target: 'http://127.0.0.1:3001', changeOrigin: true }));
-app.use('/api/watches/:wid/summary', proxy({ target: 'http://127.0.0.1:3002', changeOrigin: true }));
-app.use('/api/watches/:wid/details', proxy({ target: 'http://127.0.0.1:3003', changeOrigin: true }));
-app.use('/api/watches/:wid/reviews', proxy({ target: 'http://127.0.0.1:3004', changeOrigin: true }));
+app.use('/api/watches/:wid/photos', proxy({ target: 'http://54.211.73.211', changeOrigin: true }));
+app.use('/api/watches/:wid/summary', proxy({ target: 'http://54.87.165.253', changeOrigin: true }));
+app.use('/api/watches/:wid/details', proxy({ target: 'http://13.57.38.154', changeOrigin: true }));
+app.use('/api/watches/:wid/reviews', proxy({ target: 'http://18.218.241.2', changeOrigin: true }));
 
 
 app.listen(PORT, () => console.log(`Express Proxy listening on port ${PORT}`));
